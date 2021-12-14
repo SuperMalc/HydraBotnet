65982e67afb35317dd3568938fa7aa33cc849b7a
Malcolm Mami <62724596+SuperMalc@users.noreply.github.com>
2021-12-14 23:57:11 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-"""HTTP server classes.
+"""[MODDED VERSION OF THE ORIGINAL HTTP SERVER CLASSES]
+HTTP server classes.
 
 Note: BaseHTTPRequestHandler doesn't implement any HTTP request; see
 SimpleHTTPRequestHandler for simple implementations of GET, HEAD and POST,
